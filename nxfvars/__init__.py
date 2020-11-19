ba194cdf6a93a6eb83311d53dc5325a6d8e5738d
Gregor Sturm <mail@gregor-sturm.de>
2020-11-19 12:41:36 +0100

    Add boilerplate for setuptoos_scm and package metadata

--- nxfvars/__init__.py
+++ nxfvars/__init__.py
@@ -1,3 +1,5 @@
+"""Access nextflow variables from Python"""
 from .nxf_var_handler import NxfVarHandler
+from ._metadata import __version__, __author__, __email__, within_flit
 
 nxf = NxfVarHandler()
